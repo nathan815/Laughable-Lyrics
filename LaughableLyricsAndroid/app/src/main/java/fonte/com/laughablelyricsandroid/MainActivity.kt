650e803f0029cbfbe475976a75c955b2b1c4824c
Mark Fonte <mark@fonte.com>
2019-01-19 04:02:45 -0500

    init

---
+++ LaughableLyricsAndroid/app/src/main/java/fonte/com/laughablelyricsandroid/MainActivity.kt
@@ -0,0 +1,19 @@
+package fonte.com.laughablelyricsandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fonte.com.laughablelyricsandroid.ui.home.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ LaughableLyricsAndroid/app/src/main/java/fonte/com/laughablelyricsandroid/ui/home/MainViewModel.kt
@@ -0,0 +1,7 @@
+package fonte.com.laughablelyricsandroid.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
