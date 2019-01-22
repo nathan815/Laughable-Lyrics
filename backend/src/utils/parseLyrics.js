585882decfcb0220d2bfecb0d0395439ca708edd
Nathaniel Johnson <nathancj224@gmail.com>
2019-01-21 20:27:56 -0500

    Improved lyric parser regex

--- backend/src/utils/parseLyrics.js
+++ backend/src/utils/parseLyrics.js
@@ -1,6 +1,6 @@
 function parseLyrics(lyrics, removeAllNewLines) {
     return lyrics
-        .replace(/\[((Pre-)?Chorus|Verse|Bridge|Outro|OUTRO|Intro|INTRO)( [0-9]+)?]\]/g, '')
+        .replace(/\[.+\]/g, '')
         .replace(/^[(\n)]+/, '')
         .replace(/[(\n)]+/g, removeAllNewLines ? ' ' : '\n ');
 }
